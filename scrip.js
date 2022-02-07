e71d52936b0003625ad493846331b318aaea7ed3
CDwee <96454637+CDwee@users.noreply.github.com>
2022-02-06 18:01:13 -0600

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -35,5 +35,31 @@ while (rep <= 10) {
     rep++;
 };
 
-
 // Ended at 2:58 2-6-2022
+
+// 2nd hour 2-6-2022 
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log("Loop is about to end...");
+}
+
+// Cdoing challenge #4 
+
+
+const calcTip = function (bills) {
+    return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 110, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+// ended at 6:00 2-6-2022
